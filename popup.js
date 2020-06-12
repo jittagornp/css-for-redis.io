c0d4028713f8d4a697446f0f54989c802ec686f8
jittagornp <jittagornp@gmail.com>
2020-06-12 21:33:12 +0700

    refresh css with version (current date)

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ chrome.tabs.executeScript(null, {
             styleSheet.id = styleId;
             styleSheet.type = 'text/css';
             styleSheet.rel = 'stylesheet';
-            styleSheet.href = 'https://raw.githubusercontent.com/jittagornp/css-for-redis.io/master/redis.io.css';
+            styleSheet.href = 'https://raw.githubusercontent.com/jittagornp/css-for-redis.io/master/redis.io.css?v=' + new Date().getTime();
             document.head.appendChild(styleSheet);
         }
     `
